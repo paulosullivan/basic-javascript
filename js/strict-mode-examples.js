dea56b44b3fe2aaa148e8b4b5025ccdd5c1f30bb
paul_os\posullivan <paul_os2002@yahoo.co.uk>
2018-07-21 12:55:33 +0100

    Strict Mode

---
+++ js/strict-mode-examples.js
@@ -0,0 +1,9 @@
+"use strict";
+
+x = 10; // This will cause an error because x is not declared
+
+function myFunction() {
+    y = 20; // This will also cause an error because y is not declared
+}
+
+// You can also enable this feature for a single function by placing "use strict" as the first line of the function.
